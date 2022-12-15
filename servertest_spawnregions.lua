6e801cc4fd900bffd41404ed9d92617148c9db3e
DESKTOP\lutre <lutree@outlook.kr>
2022-12-16 03:45:57 +0900

    디스코드 설정 제외 rv 인테리어 맵 추가

--- servertest_spawnregions.lua
+++ servertest_spawnregions.lua
@@ -4,5 +4,6 @@ function SpawnRegions()
 		{ name = "Riverside, KY", file = "media/maps/Riverside, KY/spawnpoints.lua" },
 		{ name = "Rosewood, KY", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
 		{ name = "West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
+		{ name = "vehicle_interior", file = "media/maps/vehicle_interior/spawnpoints.lua" },
 	}
 end
